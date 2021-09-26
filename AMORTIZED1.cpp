3434b72e01870db3f5a7df59753ea6bf4091d61d
yshca <78138916+yshca@users.noreply.github.com>
2021-09-26 14:49:49 +0530

    DAY-1(Que.1)

---
+++ AMORTIZED1.cpp
@@ -0,0 +1,9 @@
+//the time complexity of the following code is:
+
+        int j = 0;
+        for(int i = 0; i < n; ++i) {
+            while(j < n && arr[i] < arr[j]) {
+                j++;
+            }
+        }
+// O(n)
